45ead40a59f07a107f425512d0b073529c6a4787
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-10-24 16:24:17 +0200

    feat: add dynamic statistics to homepage and about page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 
+from jobs.models import Job
 from listings.models import Listing
+from users.models import Account
 
 
 class HomeView(TemplateView):
@@ -10,14 +12,38 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["listings"] = Listing.objects.order_by("id")[:3]
         context["listings"] = Listing.objects.select_related("user").order_by("id")[:3]
+
+        context["total_students"] = Account.objects.filter(
+            role_level=Account.Role.STUDENT
+        ).count()
+        context["total_tutors"] = Account.objects.filter(
+            role_level__gte=Account.Role.TUTOR
+        ).count()
+        context["total_mentors"] = Account.objects.filter(
+            role_level__gte=Account.Role.MENTOR
+        ).count()
+        context["open_jobs"] = Job.objects.filter(status=Job.Status.OPEN).count()
+
         return context
 
 
 class AboutView(TemplateView):
-    template_name = "about.html"  # home.html
+    template_name = "about.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["listings"] = Listing.objects.order_by("id")[:3]
         context["listings"] = Listing.objects.select_related("user").order_by("id")[:3]
+
+        context["total_students"] = Account.objects.filter(
+            role_level=Account.Role.STUDENT
+        ).count()
+        context["total_tutors"] = Account.objects.filter(
+            role_level__gte=Account.Role.TUTOR
+        ).count()
+        context["total_mentors"] = Account.objects.filter(
+            role_level__gte=Account.Role.MENTOR
+        ).count()
+        context["open_jobs"] = Job.objects.filter(status=Job.Status.OPEN).count()
+
         return context
